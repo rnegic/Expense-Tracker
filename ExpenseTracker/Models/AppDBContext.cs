e329c88d1ce09a0d78ad392c62b1ddb75ed85f6b
rnegic <arnego666@gmail.com>
2024-05-25 23:45:21 +0300

    DB context added

---
+++ ExpenseTracker/Models/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerNew.Models
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
