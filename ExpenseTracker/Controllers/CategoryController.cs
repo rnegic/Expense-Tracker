e38cb23738f47ec51f7fe301ead9f09df4c0c26f
rnegic <arnego666@gmail.com>
2024-05-26 00:02:40 +0300

    Updated program.cs/appsettings so database is connected

--- ExpenseTracker/Controllers/CategoryController.cs
+++ ExpenseTracker/Controllers/CategoryController.cs
@@ -52,8 +52,6 @@ namespace ExpenseTrackerNew.Controllers
         }
 
         // POST: Category/AddOrEdit
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddOrEdit([Bind("CategoryId,Title,Icon,Type")] Category category)
--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,11 +1,18 @@
+using ExpenseTrackerNew.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//DI Database
+builder.Services.AddDbContext<AppDBContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ExpenseTrackerDB")));
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VVhjQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0VhUH5ecn1XQWJaU0E=");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -18,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Category}/{action=Index}/{id?}");
 
 app.Run();
